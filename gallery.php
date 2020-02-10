985ed8d82790b44e35522c71438931de3eeb842a
Alex Tran <quyen137@csu.fullerton.edu>
2020-02-10 15:57:29 -0800

    Update Gallery

--- gallery.php
+++ gallery.php
@@ -7,6 +7,8 @@
   // This line of code will convert the ttime from html input type"date" to store in php varible
   // https://stackoverflow.com/questions/30243775/get-date-from-input-form-within-php
   $new_date = date('Y-m-d', strtotime($_POST['dateTaken']));
+  $fileName = $_POST('uploadFile');
+  echo "$fileName";
   $document_root = $_SERVER['DOCUMENT_ROOT'];
   $outputstring = $new_date."\t".$photo."\t"
                   .$name."\t".$place."\n";
